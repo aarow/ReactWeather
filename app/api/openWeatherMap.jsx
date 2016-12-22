79f7c25f692df955c4d09d94de00417f026865d1
Aaron Neville <aaron@designliftoff.com>
2016-12-22 13:31:19 -0800

    initial commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,22 @@
+var axios = require('axios');
+
+const API_KEY = 'ef9ffaefecf42fdfb77e4f1fd18f4a41';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=';
+
+module.exports = {
+    getTemp : function(location) {
+        var encodedLocation = encodeURIComponent(location);
+        var requestUrl = `${OPEN_WEATHER_MAP_URL}${API_KEY}&q=${encodedLocation}`;
+
+        return axios.get(requestUrl).then(function(response){
+            if(response.data.cod && response.data.message) {
+                throw new Error(response.data.message);
+            } else {
+                return response.data.main.temp;
+            }
+        }, function(response){
+            throw new Error(response.data.message)
+        });
+        
+    }
+}
