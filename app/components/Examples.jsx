345e71459995ca10f17c46597a4d7c3b5b68f064
Aaron Neville <aaron@designliftoff.com>
2016-12-27 15:16:53 -0800

    add grid system and weather examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router')
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>something new!!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to="/?location=San Francisco, CA" >San Francisco, CA</Link>
+        </li>
+        <li>
+          <Link to="/?location=London, UK" >London, UK</Link>
+        </li>
+      </ol>
     </div>
   )
 }
